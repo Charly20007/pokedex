15315dfd1c6bb91942ba4647351f22a6af32b0b8
unknown <cmarcos@americatv.pe>
2024-07-16 17:44:14 -0600

    fix

--- src/components/molecules/PokemonCard.jsx
+++ src/components/molecules/PokemonCard.jsx
@@ -14,8 +14,7 @@ const PokemonCard = ({ name, url }) => {
     <Link to={`/pokemon/${pokemonId}`} className="max-w-xs rounded overflow-hidden shadow-lg my-2 bg-white">
       <img className="w-full h-48 object-contain p-4" src={imageUrl} alt={name} />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 capitalize">{name}</div>
-        <p className="text-gray-700 text-base">ID: {pokemonId}</p>
+        <div className="font-bold text-center text-xl mb-2 capitalize">{name}</div>
       </div>
     </Link>
   );
